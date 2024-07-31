ff11b23befef8d32673ba0bfb4386ebbd7040037
Usama Irfan <usama.irfan9797@gmail.com>
2024-08-01 03:00:21 +0500

    ci: added pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'your-app-name',
+      script: 'serve',
+      env: {
+        PM2_SERVE_PATH: './build',
+        PM2_SERVE_PORT: 8080, // you can choose any port
+        PM2_SERVE_SPA: 'true', // if it's a Single Page Application
+        PM2_SERVE_HOMEPAGE: '/index.html', // default entry point
+      },
+    },
+  ],
+};
