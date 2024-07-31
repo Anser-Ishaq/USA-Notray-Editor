a71d46258ea401c1dfb23f43e8d2ecf44287d6b1
Usama Irfan <usama.irfan9797@gmail.com>
2024-08-01 03:04:35 +0500

    ci: changed pm2 port

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
       script: 'serve',
       env: {
         PM2_SERVE_PATH: './build',
-        PM2_SERVE_PORT: 8080, // you can choose any port
+        PM2_SERVE_PORT: 5173, // you can choose any port
         PM2_SERVE_SPA: 'true', // if it's a Single Page Application
         PM2_SERVE_HOMEPAGE: '/index.html', // default entry point
       },
