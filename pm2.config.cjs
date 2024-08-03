1884e7636109dcb8e618463e5ba2e8d0b9b971e2
Usama Irfan <usama.irfan9797@gmail.com>
2024-08-03 05:14:59 +0500

    fix pm2 config

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: 'notary-frontend-react',
       script: 'serve',
       env: {
-        PM2_SERVE_PATH: './build',
+        PM2_SERVE_PATH: './dist',
         PM2_SERVE_PORT: 5173, // you can choose any port
         PM2_SERVE_SPA: 'true', // if it's a Single Page Application
         PM2_SERVE_HOMEPAGE: '/index.html', // default entry point
